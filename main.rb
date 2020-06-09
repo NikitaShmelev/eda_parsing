434d94ed3e62042b50295bf99812fb9b76f3a8bb
Nikita Shmelyov <Shmelev507@gmail.com>
2020-06-10 02:31:42 +0300

    parse imgs

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
-# frozen_string_literal: true
-require 'csv'
+# # frozen_string_literal: true
 require 'nokogiri'
 require 'curb'
+require "sqlite3"
+require 'rubygems'
+require 'image_downloader'
+
+db = SQLite3::Database.new "test.sqlite3"
+db.execute("CREATE TABLE IF NOT EXISTS food_sector (id INTEGER PRIMARY KEY AUTOINCREMENT, sector_name string)")
+db.execute(
+    "CREATE TABLE IF NOT EXISTS recipes (id INTEGER PRIMARY KEY AUTOINCREMENT, food_sector_id INTEGER, "+
+    +"sector_name string, discription string, create_time string, source string)")
+db.execute("CREATE TABLE IF NOT EXISTS steps (id INTEGER PRIMARY KEY AUTOINCREMENT, primary_id INTEGER, image_url string, text string)")
 
 url = 'https://eda.ru/recepty'
 
@@ -9,8 +18,7 @@ url = 'https://eda.ru/recepty'
 puts('Parsing categories...')
 
 categories = {}
-
-
+$sector_count = 0
 doc = Nokogiri::HTML(Curl.get(url).body_str)
 # print doc
 links = doc.search('li.seo-footer__list-title  a').map { |link| link['href'] }
@@ -18,9 +26,8 @@ links = doc.search('li.seo-footer__list-title  a').map { |link| link['href'] }
 
 def take_links(category_name, doc, link, index)
     links_on_page = doc.search("h3[@class='horizontal-tile__item-title item-title'] a").map { |link| link['href'] }
-    links_on_page.each_with_index do |link_on_page|
+    links_on_page.each do |link_on_page|
         if !link_on_page.include?(category_name)
-            
             links_on_page.delete(link_on_page)            
         end
     end
@@ -29,9 +36,12 @@ end
 
 
 links.each_with_index do |link, index|
-    category_name = link[23..-1] # category name
-    puts "\n"+ category_name
+    category_name = link[23..-1]
     categories[category_name] = {}
+    # db.execute("INSERT INTO food_sector (sector_name) 
+    #         VALUES (?)", [category_name])
+    $sector_count += 1
+    puts category_name
     doc = Nokogiri::HTML(Curl.get(link).body_str)
     urls = take_links(category_name, doc, link, index)
     links_on_page = Array.new()
@@ -41,7 +51,6 @@ links.each_with_index do |link, index|
     link = link+'?page='+'2'
     i = 2
     until urls.empty?
-        puts link
         doc = Nokogiri::HTML(Curl.get(link).body_str)
         urls = take_links(category_name, doc, link, index)
         urls.each do |t|
@@ -50,80 +59,41 @@ links.each_with_index do |link, index|
         i += 1
         link = link[0..link.index('=')] + i.to_s
     end
-    # puts links
-    
-    puts links_on_page.length
 
 
     links_on_page.each do |recipe_url|
         doc = Nokogiri::HTML(Curl.get('https://eda.ru'+recipe_url).body_str)
         recipe_name = doc.search("h1[@class='recipe__name g-h1']").xpath('text()')
-        CSV.open("#{category_name}.csv", 'wb') do |csv|
-            csv << %w[recipe_name kitchen portions portions food_parts]
-            kitchen = doc.search("div.recipe__title ul[@class='breadcrumbs'] li a").map { |item| item.xpath('text()') }.last.to_s
-            portions = doc.search("input[@class='portions-control__count g-h6 js-portions-count js-tooltip']").map { |item| item['value'] }.first.to_s
-            food_part = doc.search("div.ingredients-list__content p[@class='ingredients-list__content-item content-item js-cart-ingredients']")#.map { |item| item['value'] }
-            food_parts = ''
-            food_part.each do |item|
-                name =  item.search("span[@class='js-tooltip js-tooltip-ingredient']").map { |item| item.xpath('text()') }
-                amount = item.search("span[@class='content-item__measure js-ingredient-measure-amount']").map { |item| item.xpath('text()') }.to_s
-                food_parts += "#{name} #{amount}\n"    
-            end
-            puts "#{recipe_name}, #{kitchen}, #{portions}, #{food_parts}"
-            csv << ["#{recipe_name}, #{kitchen}, #{portions}, #{food_parts}"]
+        kitchen = doc.search("div.recipe__title ul[@class='breadcrumbs'] li a").map { |item| item.xpath('text()') }.last.to_s
+        portions = doc.search("input[@class='portions-control__count g-h6 js-portions-count js-tooltip']").map { |item| item['value'] }.first.to_s
+
+        discription = doc.search("p[@class='recipe__description layout__content-col corner-label__gold-thousand-mod']").xpath('text()')
+        puts discription
+        food_part = doc.search("div.ingredients-list__content p[@class='ingredients-list__content-item content-item js-cart-ingredients']")#.map { |item| item['value'] }
+        food_parts = ''
+        food_part.each do |item|
+            name =  item.search("span[@class='js-tooltip js-tooltip-ingredient']").map { |item| item.xpath('text()') }.last.to_s
+            amount = item.search("span[@class='content-item__measure js-ingredient-measure-amount']").map { |item| item.xpath('text()') }.last.to_s
+            food_parts += "#{name} #{amount}\n"    
         end
+        # steps
+        instruction = doc.search("div.recipe__instruction li[@class='instruction clearfix js-steps__parent print-preview']")#.map { |item| item.xpath('text()') }
+        discriptions = instruction.search("span[@class='instruction__description js-steps__description ']").map { |item| item.xpath('text()').to_s } # discriptions
+        imgs = instruction.search("img[@class='g-print-visible']").map{ |i| 'https:'+ i['src'] }.uniq # imgs
+        begin  
+            Dir.mkdir recipe_name.to_s
+        rescue
+        end 
+        puts imgs_urls
+        imgs.each_with_index do |photo, number|
+            File.open("#{recipe_name.to_s}/#{number}", "wb") do |f|
+                f.write(open(photo).read)
+            end
+        end
+
+        break
     end
     break
 end
-
-
-# links_on_pages.push(links)
-# i = 2
-
-# until links.empty?
-#   # puts url
-#   if url.include?('/?p=') && url[-1] != '/'
-#     url = url[0, url.length - 1] + (url[-1].to_i + (2 - 1)).to_s
-#   elsif url[-1] == '/'
-#     url = url + '?p=' + i.to_s
-#   elsif !url.include?('/?p=') && url[-1] != '/'
-#     doc = Nokogiri::HTML(Curl.get(url).body_str)
-#     links = doc.search('div.pro_outer_box a.product-name').map { |link| link['href'] }
-#     links_on_pages.push(links)
-#     break 
-#   end
-
-#   doc = Nokogiri::HTML(Curl.get(url).body_str)
-#   links = doc.search('div.pro_outer_box a.product-name').map { |link| link['href'] }
-#   links_on_pages.push(links)
-#   i += 1
-# end
-
-# puts "Page count: #{links_on_pages.length - 1}"
-
-# CSV.open("#{csv_name}.csv", 'wb') do |csv|
-#   puts 'CSV file has been created'
-
-#   csv << %w[Name Price Image]
-
-#   links_on_pages[0, links_on_pages.length - 1].each_with_index do |links, item_index|
-    
-#     puts "Searching on page №#{item_index + 1}"
-
-#     links.each do |link|
-#       doc = Nokogiri::HTML(Curl.get(link).body_str)
-      
-#       name = doc.search('h1.product_main_name').xpath('text()')
-#       prices = doc.search('span.price_comb').map { |item| item.xpath('text()') }
-#       image = doc.search('img#bigpic').map { |item| item['src'] }
-      
-#       doc.search('span.radio_label').map { |item| item.xpath('text()') }.each_with_index do |item, price_item|
-#         csv << ["#{name} #{item}", prices[price_item], image[0]]
-#       end
-#     end
-
-#     puts "Page №#{item_index + 1} finished"
-#   end
-# end
 
 puts 'Work completed successfully'
